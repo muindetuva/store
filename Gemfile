f0df7aae0a17b8a32f6eac7813d55a9bcc560d81
Alfred Tuva <muindetuva@gmail.com>
2025-02-14 15:09:36 +0300

    learn: crud

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "solargraph", "~> 0.51.2", :group => :development
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "/products", to: "products#index"
+  # post "/products", to: "products#create"
+  # get "/products/:id", to: "products#show"
+  root "products#index"
+  resources :products
 end
